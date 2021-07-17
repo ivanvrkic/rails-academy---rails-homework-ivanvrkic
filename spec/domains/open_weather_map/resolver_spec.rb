c3138cae398ed772c0ad99bf76c82885c31ea60b
Ivan Vrkic <kontakt@ivanvrkic.com>
2021-07-17 21:07:43 +0200

    fixed resolver spec

--- spec/domains/open_weather_map/resolver_spec.rb
+++ spec/domains/open_weather_map/resolver_spec.rb
@@ -1,12 +1,12 @@
-RSpec.describe Resolver, :resolver do
+RSpec.describe OpenWeatherMap::Resolver, :resolver do
   it 'returns correct id for a known city' do
-    expect(OpenWeatherMap::Resolver.city_id('Zagreb')).to eq(3_186_886)
-    expect(OpenWeatherMap::Resolver.city_id('New York')).to eq(5_128_638)
-    expect(OpenWeatherMap::Resolver.city_id('Frankfurt am Main')).to eq(2_925_533)
+    expect(described_class.city_id('Zagreb')).to eq(3_186_886)
+    expect(described_class.city_id('New York')).to eq(5_128_638)
+    expect(described_class.city_id('Frankfurt am Main')).to eq(2_925_533)
   end
 
   it 'returns nil for unknown city' do
-    expect(OpenWeatherMap::Resolver.city_id('Zabreg')).to eq(nil)
-    expect(OpenWeatherMap::Resolver.city_id('xxxxx')).to eq(nil)
+    expect(described_class.city_id('Zabreg')).to eq(nil)
+    expect(described_class.city_id('xxxxx')).to eq(nil)
   end
 end
