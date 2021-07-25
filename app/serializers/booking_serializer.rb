86a6c32651f7308cafb15f7b86f0c2d6e1d13f08
Ivan Vrkic <kontakt@ivanvrkic.com>
2021-07-25 12:35:35 +0200

    added serializer associations

--- app/serializers/booking_serializer.rb
+++ app/serializers/booking_serializer.rb
@@ -2,8 +2,9 @@ class BookingSerializer < Blueprinter::Base
   identifier :id
   field :no_of_seats
   field :seat_price
-  field :flight_id
-  field :user_id
   field :created_at
   field :updated_at
+
+  association :flight, blueprint: FlightSerializer
+  association :user, blueprint: UserSerializer
 end
--- app/serializers/flight_serializer.rb
+++ app/serializers/flight_serializer.rb
@@ -5,7 +5,7 @@ class FlightSerializer < Blueprinter::Base
   field :base_price
   field :departs_at
   field :arrives_at
-  field :company_id
   field :created_at
   field :updated_at
+  association :company, blueprint: CompanySerializer
 end
