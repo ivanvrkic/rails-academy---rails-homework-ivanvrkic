cf809edba6be13613b897c2d8aa1e8ba7dcc16e5
Ivan Vrkic <kontakt@ivanvrkic.com>
2021-08-12 09:45:30 +0200

    fix

--- app/queries/companies_query.rb
+++ app/queries/companies_query.rb
@@ -9,7 +9,7 @@ class CompaniesQuery
     relation.left_outer_joins(flights: :bookings)
             .group('companies.id, flights.company_id')
             .select('companies.id,
-                     COALESCE(AVG(bookings.seat_price),0)::DOUBLE PRECISION
+                     COALESCE(AVG(bookings.seat_price)::DOUBLE PRECISION,0)
                      as average_price_of_seats,
                      COALESCE(SUM(bookings.no_of_seats),0) as total_no_of_booked_seats,
                      COALESCE(SUM(bookings.no_of_seats*bookings.seat_price),0) as total_revenue')
