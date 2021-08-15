dac4856e1070aae488a4ebb289972f83ef7c8929
Ivan Vrkic <kontakt@ivanvrkic.com>
2021-08-15 18:16:24 +0200

    fix

--- spec/factories/flight.rb
+++ spec/factories/flight.rb
@@ -2,8 +2,8 @@ FactoryBot.define do
   factory :flight do
     company { create(:company) }
     sequence(:name) { |n| "Flight10#{999 - n}" }
-    sequence(:departs_at) { |n| (n.days.from_now - 1.hour).to_s }
-    sequence(:arrives_at) { |n| ((n + 1).days.from_now - 12.hours).to_s }
+    sequence(:departs_at) { |n| ((n*2).days.from_now - 1.hour).to_s }
+    sequence(:arrives_at) { |n| ((n*2).days.from_now + 1.hours).to_s }
     base_price { 10 }
     no_of_seats { 10 }
   end
